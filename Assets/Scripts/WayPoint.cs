c0bbab7e8d5e39aef46be5328089366e622a28b2
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-04-18 00:52:45 -0400

    added waypoint editor

---
+++ Assets/Scripts/WayPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+
+    public List<WayPoint> connections = new List<WayPoint>();
+    
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AssignTarget(PathAI pathAi)
+    {
+        //pathAi.target = 
+        
+        
+    }
+
+    Vector2 GetPosition()
+    {
+        return new Vector2(transform.position.x,transform.position.z);
+    }
+}
