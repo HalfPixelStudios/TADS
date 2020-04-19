dc76932bd567cdac6586b614f8878338e2f6f29a
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-04-19 14:13:35 -0400

    d

--- Assets/Scripts/Controllers/RobberController.cs
+++ Assets/Scripts/Controllers/RobberController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class RobberController : Possesable
 {
-    
-    
+    public override void DefaultBehavior()
+    {
+        
+    }
+
+    public override void PossessedBehavior()
+    {
+        Vector2 clickPos = GlobalContainer.Global.screenManager.GetScreenClick();
+        transform.LookAt(new Vector3(clickPos.x,0,clickPos.y));
+        transform.rotation=Quaternion.Euler(0,transform.rotation.eulerAngles.y,0);
+        
+        
+    }
 }
